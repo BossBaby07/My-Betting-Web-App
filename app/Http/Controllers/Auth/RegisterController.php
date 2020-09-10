e4b11bbb3711a5d1087363f760db74f5e0059531
BossBaby07 <taufiq.hasib@northsouth.edu>
2020-09-10 09:12:40 +0600

    commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'user_name' => ['required', 'string', 'min:3', 'max:255'],
+            'referrer_name' => ['required', 'string', 'min:3', 'max:255'],
         ]);
     }
 
@@ -68,6 +70,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'user_name' => $data['user_name'],
+            'referrer_name' => $data['referrer_name'],
+            'wallet' => 0,
         ]);
     }
 }
